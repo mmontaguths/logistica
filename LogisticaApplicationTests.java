9489cf86b4c43ca1aad356ef7dc59c1b692fad54
prochel <106088594+mmontaguths@users.noreply.github.com>
2022-07-06 19:14:38 -0500

    Add files via upload

---
+++ LogisticaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjms.envios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
